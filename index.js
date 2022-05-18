69a014c51c2b3f0d6140b85b23a4b1d4d55cf695
GhostMarket <57845020+narleyto@users.noreply.github.com>
2022-05-18 07:41:25 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var mineflayer = require('mineflayer')
 
 var bot = mineflayer.createBot({
-  host: "narpixel.tk",
-  username: "Narpixel",
-  version: "1.16"
+  host: "bestserver.net", //your server ip
+  username: "iam7/24bot", //bot name
+  version: "1.18" //server version
 })
 
 
 bot.on('login', async () => {
-    console.log(' bot aktif ')
+    console.log(' bot online ')
 })
 
